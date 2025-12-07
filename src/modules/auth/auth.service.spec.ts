60ba4250a87e1fd9204a31b082f6bc9c8dd4f24a
Pierre Schnell <pierreschnell@hotmail.com>
2025-12-07 23:38:35 +0100

    derniere modif avant presentation cda 3

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/unbound-method */
+// "Dans ce fichier, ignore les avertissements sur les méthodes non liées"
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from '../users/users.service';
